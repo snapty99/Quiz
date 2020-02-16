4092ab7abcbd39a9439f50e73e050bf66b3ba32d
Konlachan.S <52999675+snapty99@users.noreply.github.com>
2020-02-16 16:31:51 +0700

    Add files via upload

---
+++ Quiz3-2-2.php
@@ -0,0 +1,13 @@
+<?php
+$marks = array(
+  "mohammad"=>array("physics"=>35,"maths"=>30,"chemistry"=>39),
+  "qadir"=>array("physics"=>30,"maths"=>32,"chemistry"=>29),
+  "zara"=>array("physics"=>31,"maths"=>22,"chemistry"=>39)
+);
+foreach($marks as $name => $class){
+  foreach($class as $point){
+    echo $point."<br>";
+  }
+}
+
+?>
